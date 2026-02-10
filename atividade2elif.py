a2cf43dacd6e2cca0a764cf1d1d06a53c05855c9
WawaCat-TheGameDev2462 <pedro.ilberte.studiogames@gmail.com>
2026-02-10 19:00:37 -0300

    Create atividade2elif.py
    atividade 2 lol

---
+++ atividade2elif.py
@@ -0,0 +1,11 @@
+nota_aluno = int(input("sua nota: "))
+if nota_aluno >= 9:
+    print("A")
+elif nota_aluno >= 8:
+    print("B")
+elif nota_aluno >= 7:
+    print("C")
+elif nota_aluno >= 6:
+    print("D")
+else:
+    print("F")
