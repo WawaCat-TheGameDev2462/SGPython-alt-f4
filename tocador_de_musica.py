d40d36780417da0d70faaa0378a6307274d87e18
WawaCat-TheGameDev2462 <pedro.ilberte.studiogames@gmail.com>
2026-02-28 19:00:27 -0300

    Add basic music player functionality

---
+++ tocador_de_musica.py
@@ -0,0 +1,7 @@
+
+musica = input("qual musica voce quer ouvir: ")
+print(f"tocando {musica}")
+if musica == musica and musica == musica or not (musica ==  musica):
+    print("tocando musica")
+
+    
