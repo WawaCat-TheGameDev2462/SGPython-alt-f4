a6ad5058f2258fab206f750ea193c0e32172d70b
WawaCat-TheGameDev2462 <pedro.ilberte.studiogames@gmail.com>
2026-04-25 17:44:18 -0300

    burp gosling simulator

---
+++ atividade2sequenciaFOR_RANGE.py
@@ -0,0 +1,6 @@
+
+import time
+numero_contado = int(input("voce quer contar de 1 ate quanto? "))
+for numero_contado1 in range(numero_contado):
+    print("numero contado: " + str(numero_contado1 + 1))
+    time.sleep(0.5)
