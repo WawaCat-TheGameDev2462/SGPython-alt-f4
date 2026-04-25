89a9613626f97c95fe13e5a2104c001f00f42ac0
WawaCat-TheGameDev2462 <pedro.ilberte.studiogames@gmail.com>
2026-04-25 18:14:41 -0300

    jogos legalis i horivvies

---
+++ atividade3sequenciaFORlistaGAMES.py
@@ -0,0 +1,14 @@
+jogos = ["screw drivers","wobbledogs", "gang beasts", "rock simulator", "blood strike" ]
+for jogo in jogos:
+    if jogo == jogos [0]:
+        print(jogo + " jogo favorito em que voce constroi oque quiser enquanto tambem pode usar multijogador e missoes")
+    elif jogo == jogos [1]:
+        print(jogo + " jogo calmo e interessante para relaxar em que voce pode criar cachorros ou sei la oque são essas fofuras e fazer aberracoes tambem ")
+    elif jogo == jogos [2]:
+        print(jogo + " jogo caotico em que voce luta em mapas em varios lugares e cenarios contra ondas de inimigos ou contra amigos")
+    elif jogo == jogos [3]:
+        print(jogo + " jogo em que voce simula a vida de uma pedra, sem movimento, so movimento de camera e relaxamento, jogo 10/10")
+    elif jogo == jogos [4]:
+        print(jogo + " JOGO ABSOLUTAMENTE HORRIVEL QUE NINGUEM DEVE JOGAR EU ESPERO QUE VOCE SEJA TRANSFORMADO EM UMA PEDRA SEJA ESMAGADO SEJA JOGADO NO MAR E DISSOLVER COMO PO E DEPOIS SER FILTRADO E SER MANDADO PARA A SEXTA CAMADA MAIS FUNDA DO INFERNO")
+    else:
+        print("parabens voce quebrou o codigo de alguma forma")
